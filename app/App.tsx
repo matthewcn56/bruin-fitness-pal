18465020fe296fb4dad9f6fd00901f4b1fec37ab
matthewcn56 <matthewcn56@gmail.com>
2021-12-14 21:48:02 -0800

    backend authentication setup

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as Google from "expo-auth-session/providers/google";
+import {
+  EXPO_AUTH_CLIENT_ID,
+  IOS_CLIENT_ID,
+  EXPO_AUTH_CLIENT_SECRET,
+} from "@env";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: EXPO_AUTH_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    clientSecret: EXPO_AUTH_CLIENT_SECRET,
+  });
+
+  useEffect(() => {
+    console.log("Vars: " + EXPO_AUTH_CLIENT_ID + "," + IOS_CLIENT_ID);
+  }, []);
+  useEffect(() => {
+    if (response && response.type === "success") {
+      console.log(response);
+    }
+  }, [response]);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button onPress={() => promptAsync()} title="LogIn!" />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +37,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+//use the sub as hash!
--- server/auth_routes.py
+++ server/auth_routes.py
@@ -2,8 +2,54 @@ import os
 import datetime
 # from posix import environ
 from route_config import *
-from flask import make_response
+from flask import make_response, jsonify, request
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from functools import wraps
 
+
+def auth_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        if 'token' in request.headers:
+            token = request.headers['access-token']
+        if not token:
+            return make_response('Invalid Auth Token', 401)
+        try:
+            idinfo = id_token.verify_oauth2_token(token, requests.Request(), os.environ['EXPO_AUTH_CLIENT_ID'])
+            uid = idinfo['sub']
+            db.users.find_one_or_404({"_id":uid})
+        except:
+            return make_response('Invalid Auth Token', 401)
+        return f(uid, *args, **kwargs)
+    return decorator
+
+
+
+# use sub as hash! pass in register boolean for if should add to user db or not
 @app.route('/login', methods = ['POST'])
-def login_user():
-    return make_response(jsonify({ 'message': 'Hello there!'}),  200)
+def verify_token():
+    jsonData = request.json
+    token = jsonData['token']
+    try:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), os.environ['EXPO_AUTH_CLIENT_ID'])
+        uid = idinfo['sub']
+        email = idinfo['email']
+        name = idinfo['name']
+        picture = idinfo['picture']
+        # add user to db if not existed yet
+        existingUser = db.users.find_one({'_id':uid})
+        if not existingUser:
+            new_user = db.users.insert_one({
+                "_id": uid,
+                "email": email,
+                "name": name,
+                "picture": picture
+            })
+        #print(idinfo)
+        return make_response(jsonify({ 'user': idinfo}),  200)
+    except ValueError:
+        return make_response("Invalid Token!",  401)
+
+
